6f8eb244e2518675615da27936fe1bdad2240b1c
Ubuntu <ubuntu@ip-172-31-26-225.us-east-2.compute.internal>
2026-09-15 19:59:41 +0000

    HW2

--- myproject/customers/views.py
+++ myproject/customers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Customer
+from .serializers import CustomerSerializer
 
-# Create your views here.
+
+class CustomerListCreateView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
